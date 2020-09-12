bf49dfb7b6c6edda4a3bc03e2f59064f701e06bb
Rikuta Hiratsuka <rimuru930@gmail.com>
2020-09-13 00:49:21 +0900

    テンプレートを使ってシンプルな文字を表示

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    name = 'rikuta'
+    context = {'name': name}
+    return render(request, 'pages/index.html', context)
+
+
+def profile(request):
+    return HttpResponse('Profile!')
+
+
+def jobs(request):
+    return HttpResponse('私の仕事は〇〇です｡')
+
+
+def hobby(request):
+    return HttpResponse('My hobby is programming')
+
